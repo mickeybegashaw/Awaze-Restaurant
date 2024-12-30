04ca46919f9aabb1306690c9d3af929860e89b6c
mikiyas begashaw <mickeyjob2004@gmail.com>
2024-12-30 10:35:42 +0300

    connect to mongoDB

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,19 @@
-import express from 'express';
-import dotenv from 'dotenv';
-import cors from 'cors';
-
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import mongoose from "mongoose";
 dotenv.config();
 const app = express();
 
+app.use(cors());
+app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server is running on port ${process.env.PORT}`);
-});
+mongoose
+  .connect(process.env.MONGODB_URI)
 
+  .then(() => {
+    app.listen(process.env.PORT, () => {
+      console.log(`Server is connected to DB and running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => console.error(error.message));
