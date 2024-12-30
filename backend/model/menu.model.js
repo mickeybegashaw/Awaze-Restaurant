ec3d9b360b423310b3edc4374873849cab59853d
mikiyas begashaw <mickeyjob2004@gmail.com>
2024-12-30 10:43:00 +0300

    make the database model

---
+++ backend/model/menu.model.js
@@ -0,0 +1,13 @@
+import mongoose from "mongoose";
+
+const Schema = mongoose.Schema;
+
+const menuSchema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  price: { type: Number, required: true },
+  image: { type: String, required: true },
+})
+
+const Menu = mongoose.model("Menu", menuSchema);
+export default Menu;
