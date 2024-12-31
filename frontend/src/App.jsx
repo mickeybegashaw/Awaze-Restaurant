c95364df43d0a241d5848c848f79562280cc82d8
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-01-01 00:19:26 +0300

    edit menu component

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,9 @@
 import Header from "./components/Header";
 import Home from "./components/Home";
 import About from "./components/About";
-import Menu from "./components/menu";
 import WhyUs from "./components/WhyUs";
 import Footer from "./components/Footer";
+import Menu from "./components/theMenu"
 const App = () => {
   return (
 
