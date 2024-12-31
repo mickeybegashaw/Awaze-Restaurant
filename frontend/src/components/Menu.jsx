310052ca48eef1b874a32df0aedd7cea8adaed0b
mikiyas begashaw <mickeyjob2004@gmail.com>
2024-12-31 12:10:56 +0300

    fetch menus from the backend

--- frontend/src/components/Menu.jsx
+++ frontend/src/components/Menu.jsx
@@ -1,4 +1,25 @@
+import axios from "axios";
+import UseMenuContext from "../hooks/useMenuContext"; 
+import { useContext, useEffect } from "react";
+import { MenuContext } from "../context/menuContext";
+const baseUrl = import.meta.env.VITE_REACT_APP_API_URL;
+
 const Menu = () => {
+  const { state,dispatch } = UseMenuContext();
+
+  useEffect(() => {
+    const FetchMenu = async () => {
+      try {
+        const response = await axios.get(`${baseUrl}/api/menu`);
+        dispatch({ type: "GET_MENU", payload: response.data });
+      } catch (error) {
+        console.log(error);
+      }
+    };
+    FetchMenu();
+  }, [dispatch]);
+  
+  console.log(state);
   return (
     <section
       id="menu"
