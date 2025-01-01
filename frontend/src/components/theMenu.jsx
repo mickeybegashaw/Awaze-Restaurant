949e2b91816c56463fbe801270172520bd13cba2
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-01-01 10:54:53 +0300

    add loading state

--- frontend/src/components/theMenu.jsx
+++ frontend/src/components/theMenu.jsx
@@ -1,18 +1,23 @@
 import axios from "axios";
 import UseMenuContext from "../hooks/useMenuContext";
-import { useEffect } from "react";
+import { useEffect , useState } from "react";
+
 const baseUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
 const Menu = () => {
   const { state, dispatch } = UseMenuContext();
+  const [loading, setLoading] = useState(false);
 
   useEffect(() => {
     const FetchMenu = async () => {
       try {
+        setLoading(true);
         const response = await axios.get(`${baseUrl}/api/menu`);
         dispatch({ type: "GET_MENU", payload: response.data });
       } catch (error) {
         console.log(error);
+      }finally{
+        setLoading(false);
       }
     };
     FetchMenu();
@@ -26,6 +31,7 @@ const Menu = () => {
       <h2 className="text-5xl text-red-700 font-bold text-center font-cursive mb-5">
         Menus
       </h2>
+      {loading && <h1 className="text-2xl text-stone-400 text-center">Loading menu...</h1>}
       <div className="flex md:flex-row flex-col md:flex-wrap md:w-11/12 md:justify-center items-center text-white font-serif">
         {state &&
           state.menu.map((menu) => (
