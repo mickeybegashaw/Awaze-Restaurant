39199fcd8c0a3d115dff6f448580f2922204011a
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-01-02 00:19:28 +0300

    fiz styling and add link to heder elements

--- frontend/src/adminComponents/AdminHeader.jsx
+++ frontend/src/adminComponents/AdminHeader.jsx
@@ -1,24 +1,20 @@
 import logoImg from "../assets/Awazelogo.png";
-import { Link } from "react-scroll";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
     <div className="fixed  bg-stone-950 w-screen h-20 md:h-24 text-white flex justify-between items-center z-10">
-        <Link
-        to="home"
-        smooth={true}
-        duration={900}
-        offset={-70} 
-        className="object-contain h-24 ml-2 md:h-36 md:ml-10"
-      >
+      <Link to={"/"}>
         <img
           className="object-contain h-24 cursor-pointer ml-2 md:h-36 md:ml-10"
           src={logoImg}
           alt="Awaze restaurant logo"
         />
       </Link>
-      
-      <span className="mr-10">Back To Home page</span>
+
+      <Link to={"/"}>
+        <span className="mr-10">Back To Home page</span>
+      </Link>
     </div>
   );
 };
--- frontend/src/adminComponents/HeroAdmin.jsx
+++ frontend/src/adminComponents/HeroAdmin.jsx
@@ -1,16 +1,16 @@
 import UseMenuContext from "../hooks/useMenuContext";
-import { useState , useEffect } from "react";
+import { useState, useEffect } from "react";
 import axios from "axios";
 import { CiEdit } from "react-icons/ci";
 import { MdDelete } from "react-icons/md";
-
+import { IoMdAddCircleOutline } from "react-icons/io";
 
 const baseUrl = import.meta.env.VITE_REACT_APP_API_URL;
 
 const AdminHero = () => {
-  const { state , dispatch} = UseMenuContext();
+  const { state, dispatch } = UseMenuContext();
   const [loading, setLoading] = useState(false);
-  
+
   useEffect(() => {
     const FetchMenu = async () => {
       try {
@@ -19,7 +19,7 @@ const AdminHero = () => {
         dispatch({ type: "GET_MENU", payload: response.data });
       } catch (error) {
         console.log(error);
-      }finally{
+      } finally {
         setLoading(false);
       }
     };
@@ -32,22 +32,49 @@ const AdminHero = () => {
       className="font-serif 
       min-h-screen  w-screen flex items-center flex-col "
     >
-      <h1 className="text-red-700 font-bold text-3xl text-center mt-3">Admin panel</h1>
-      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-3 bg-stone-300 rounded-xl hover:shadow-black hover:shadow-xl w-11/12 place-items-center md:gap-10 mt-6 transition-shadow">
-      {loading && <h1 className="text-2xl text-stone-400 text-center">Loading menu...</h1>}
-        {state && state.menu.map((menu)=>(
-          <div key={menu._id} className="h-24 w-11/12 border-2 border-red-700 flex items-center rounded-xl justify-between "> 
-          <img src={menu.image.url} loading="lazy" className="h-full w-24 md:w-32 object-cover rounded-xl" alt="image of menus" />
-          <div className="flex flex-col  w-fit">
-            <p className="text-xl">{menu.title}</p>
-          </div>
-          <div className="flex flex-col gap-5 mr-5  w-fit">
-            <p><CiEdit size={30}/></p>
-            <p><MdDelete size={30} color="red"/></p>
-          </div>
-          </div>
-        ))}
-
+      <h1 className="text-red-700 font-bold text-3xl text-center mt-3">
+        Admin panel
+      </h1>
+      <div className="bg-stone-300 w-11/12 h-16 flex justify-center items-center rounded-xl mt-3">
+        <h1 className="text-xl text-stone-600 font-bold">
+          {" "}
+          <span className="inline-block">
+            <IoMdAddCircleOutline size={20}/>
+          </span>{" "}
+          Add new menu
+        </h1>{" "}
+      </div>
+      <div className="grid grid-cols-1 md:grid-cols-2 gap-5 p-3 bg-stone-300 rounded-xl hover:shadow-black hover:shadow-xl w-11/12 place-items-center md:gap-10 mt-4 transition-shadow">
+        {loading && (
+          <h1 className="text-2xl text-stone-400 text-center">
+            Loading menu...
+          </h1>
+        )}
+        {state &&
+          state.menu.map((menu) => (
+            <div
+              key={menu._id}
+              className="h-24 w-11/12 border-2 border-red-700 flex items-center rounded-xl justify-between "
+            >
+              <img
+                src={menu.image.url}
+                loading="lazy"
+                className="h-full w-24 md:w-32 object-cover rounded-xl"
+                alt="image of menus"
+              />
+              <div className="flex flex-col  w-fit">
+                <p className="text-xl">{menu.title}</p>
+              </div>
+              <div className="flex flex-col gap-5 mr-5  w-fit">
+                <p>
+                  <CiEdit size={30} />
+                </p>
+                <p>
+                  <MdDelete size={30} color="red" />
+                </p>
+              </div>
+            </div>
+          ))}
       </div>
     </section>
   );
