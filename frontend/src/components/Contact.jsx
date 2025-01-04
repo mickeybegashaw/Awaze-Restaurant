ba05b45f81322421c546a0ebb180158309f3e844
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-01-05 00:39:26 +0300

    add icons for the footer, edit style for contact

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -2,7 +2,7 @@ const Contact = () => {
   return (
     <div
       id="contact"
-      className="font-serif  pt-10 bg-stone-800 h-3/4 w-screen flex flex-col gap-5 min-h-screen"
+      className="font-serif  pt-10 bg-stone-800 h-2/3 w-screen flex flex-col gap-5 min-h-screen"
     >
       <h2 className="text-4xl md:text-5xl font-cursive text-red-700 font-bold text-center">
         Contact{" "}
@@ -23,7 +23,7 @@ const Contact = () => {
         src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3940.5711952177494!2d38.780565800000005!3d9.011544699999998!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x164b851b9f16b243%3A0x4d015b99935736b7!2zQXdhemUgUmVzdGF1cmFudCB8IEhheWEgSHVsZXQgfCDhiqDhi4vhi5wg4Yis4Yi14Ym24Yir4YqV4Ym1IHwg4YiD4YurIOGIgeGIiOGJtQ!5e0!3m2!1sen!2set!4v1736022417949!5m2!1sen!2set"
         width=""
         height=""
-        style={{ border: 0 , width: "100%" , height: "500px"}}
+        style={{ border: 0 , width: "100%" , height: "450px"}}
         allowFullScreen=""
         loading="lazy"
         referrerPolicy="no-referrer-when-downgrade"
--- frontend/src/components/Footer.jsx
+++ frontend/src/components/Footer.jsx
@@ -1,13 +1,28 @@
 import { Link } from "react-scroll";
 import logoImg from "../assets/Awazelogo.png";
+import { FaInstagram } from "react-icons/fa";
+import { FaSquarePhone } from "react-icons/fa6";
 
 const Footer = () => {
   return (
     <section className="bg-stone-950 h-fit w-screen text-white py-10">
-    
       <div className="flex flex-col items-center">
-        <img src={logoImg} alt="the awaze restaurant logo" className="h-44 w-52" />
-        <p>
+        <img
+          src={logoImg}
+          alt="the awaze restaurant logo"
+          className="h-44 w-52"
+        />
+        <div className="flex gap-5">
+          <a href="https://www.instagram.com/chef_.z/?hl=en">
+            <FaInstagram size={20} />
+          </a>
+          <a href="tel:+251">
+            {" "}
+            <FaSquarePhone size={20} />
+          </a>
+        </div>
+
+        <p className="mt-10">
           &copy; {new Date().getFullYear()} | Developed by{" "}
           <a className="text-red-700" href="https://github.com/mickeybegashaw">
             Mikiyas Begashaw
