fc9665eb4ce10c9ea54fd394a4284b3017dee49a
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-01-10 21:22:25 +0300

    add authentication middleware to menu routes for secure access

--- backend/Routes/menuRoute.js
+++ backend/Routes/menuRoute.js
@@ -1,15 +1,16 @@
 import express from 'express';
 import multer from 'multer';
 import { getAllMenu, createMenu, updateMenu, deleteMenu } from '../controler/menuController.js';
+import { requireAuth } from '../middleware/requireAuth.js';
 const upload = multer({ dest: 'uploads/' });
 const router = express.Router();
 
 router.get('/', getAllMenu);
 
-router.post('/',  upload.single('image') ,createMenu);
+router.post('/', requireAuth, upload.single('image') ,createMenu);
 
-router.patch('/:id', updateMenu);
+router.patch('/:id', requireAuth ,updateMenu);
 
-router.delete('/:id', deleteMenu);
+router.delete('/:id', requireAuth ,deleteMenu);
 
 export default router;
