61f86d6fd4b4745b0dc918643d2c564422f46945
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-01-10 21:18:52 +0300

    add login route to userRoute and implement loginController

--- backend/Routes/userRoute.js
+++ backend/Routes/userRoute.js
@@ -1,10 +1,11 @@
-import express from 'express'
-import { registerController } from '../controler/userController.js'
-const router = express.Router()
+import express from "express";
+import {
+  registerController,
+  loginController,
+} from "../controler/userController.js";
+const router = express.Router();
 
-router.post('/register', registerController)
+router.post("/register", registerController);
 
-router.post('/login', (req , res)=>{
-  res.status(200).json("heloo logmuk")
-})
-export default router
+router.post("/login", loginController);
+export default router;
