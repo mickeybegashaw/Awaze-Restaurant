9c5de02e9c3d679348e0d1d1d859ad1acb0a62a3
mikiyas begashaw <mickeyjob2004@gmail.com>
2025-01-10 23:36:30 +0300

    add user state initialization from localStorage in UserContextProvider

--- frontend/src/context/userContext.jsx
+++ frontend/src/context/userContext.jsx
@@ -1,10 +1,16 @@
-import { createContext, useState } from "react";
+import { createContext, useState , useEffect} from "react";
 export const userContext = createContext();
 
 const UserContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
+  useEffect(() => {
+    const user = JSON.parse(localStorage.getItem("user"));
+    if (user) {
+      setUser(user);
+    }
+  }, []);
   return (
-    <userContext.Provider value={{user, setUser}}>
+    <userContext.Provider value={{ user, setUser }}>
       {children}
     </userContext.Provider>
   );
